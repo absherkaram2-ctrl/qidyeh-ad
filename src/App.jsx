cbb90ac4f7d497601256c1e8cdd3e596f33e8e71
absherkaram2-ctrl <absherkaram2@gmail.com>
2026-01-14 19:23:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from "./Main_Page";
 import Login from "./Login";
 
 // export const serverRoute = 'http://localhost:8080'
-export const serverRoute = "https://tic-6gfk.onrender.com";
+export const serverRoute = "https://qidyeh-se.onrender.com";
 // export const serverRoute = "https://salmh-se2.onrender.com";
 export const token = localStorage.getItem("token");
 function App() {
